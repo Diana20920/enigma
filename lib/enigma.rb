4756bfe61af4e4b151c4c6d337889540f0dc66cc
Diana Buffone <diana.pamelaet@gmail.com>
2021-01-16 11:40:39 -0500

    Pass encrypt partly hardcoded

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -12,32 +12,50 @@ class Enigma
   end
 
   def encrypt(message, key, date)
-    expected_hash = {}
+    # expected_hash = Hash.new("")
     character_set = ("a".."z").to_a << " "
     shift_a = 3
     shift_b = 27
     shift_c = 73
     shift_d = 20
     to_encrypt = message.chars.each_slice(4).to_a
+    new_string = []
 
-    new_string = to_encrypt.map do |array_of_4|
+    to_encrypt.map do |array_of_4|
       if character_set.include?(array_of_4[0])
-        character_set.index(array_of_4[0]) + shift_a
-      elsif character_set.include?(array_of_4[1])
-        character_set.index(array_of_4[1]) + shift_b
-      elsif character_set.include?(array_of_4[2])
-        character_set.index(array_of_4[2]) + shift_c
-      elsif character_set.include?(array_of_4[3])
-        character_set.index(array_of_4[3]) + shift_d
+        index_a = character_set.index(array_of_4[0])
+        new_ind_a = (index_a + shift_a).modulo(27)
+        new_string << character_set[new_ind_a]
+        if character_set.include?(array_of_4[1])
+          index_b = character_set.index(array_of_4[1])
+          new_ind_b = (index_b + shift_b).modulo(27)
+          new_string << character_set[new_ind_b]
+          if character_set.include?(array_of_4[2])
+            index_c = character_set.index(array_of_4[2])
+            new_ind_c = (index_c + shift_c).modulo(27)
+            new_string << character_set[new_ind_c]
+            if character_set.include?(array_of_4[3])
+              index_d = character_set.index(array_of_4[3])
+              new_ind_d = (index_d + shift_d).modulo(27)
+              new_string << character_set[new_ind_d]
+      # elsif
+        # p "condition goes here"
+            end
+          end
+        end
       end
     end
-    new_string
+    {
+      encryption: new_string.join,
+      key:        key,
+      date:       date
+   }
   end
 
   def shifts
     nums = (0..9).to_a
     shifts_hash = Hash.new(0)
-
+    
 
   end
 end
