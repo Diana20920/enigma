07a41a23f2953fda00dc2676281884aed24f048a
Diana Buffone <diana.pamelaet@gmail.com>
2021-01-15 21:04:43 -0500

    Add progress for enigma class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,4 +1,18 @@
 require 'date'
 
 class Enigma
+
+  def encrypt(message, key, date)
+    expected_hash = {}
+    character_set = ("a".."z").to_a << " "
+
+    array_message = message.chars
+    array_message.map.with_index do |letter, index|
+      if character_set.include?(letter)
+        character_set.rindex(letter) # up to here, I get an array of the indeces corresponding to the message.
+      end
+    end
+  end
+
+
 end
