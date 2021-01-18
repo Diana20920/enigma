b71615c73e68f8887c2e9e954f9c2038f3d238d4
Diana Buffone <diana.pamelaet@gmail.com>
2021-01-18 01:16:48 -0500

    Update the encrypt file

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,35 +1,16 @@
-require_relative 'enigma.rb'
-# this ~works. Need to actually implement it to know for sure.
-class FileInput
+require_relative 'enigma'
+require_relative 'key'
+require_relative 'offset'
 
-  def read
-    handle = File.open(ARGV[0], 'r')
-    given_file = handle.read
-    handle.close
-  end
-end
+in_file = File.open(ARGV[0], 'r')
+to_encrypt = File.read(in_file)
 
-class FileOutput
-  attr_reader :reader
+cipher = Enigma.new(to_encrypt)
+cipher.encrypt(to_encrypt)
 
-  def initialize
-    @reader = Enigma.new(:encrypt)
-  end
+out_file = File.open(ARGV[1], 'w')
+# out_file.encrypt(in_file)
 
-  def write(text)
-    new_message = File.open(encrypted.txt, 'w')
-    new_message.write(text)
-    new_message.close
-  end
-end
-# below is a work in progress becuase it does not yet take the second file.
-
-# ruby ./lib/encrypt.rb message.txt encrypted.txt
-
-# encrypted = given_file.encrypt(given_file)
-
-# writer = File.open(ARGV[1], 'w')
-# writer.write(encrypted)
-# writer.close
-
-puts "Created 'encrypted.txt' with the key 82648 and date 240818"
+p "Created #{ARGV[0]} with the key #{cipher.key.number} and date #{cipher.date.date}"
+in_file.close
+out_file.close
