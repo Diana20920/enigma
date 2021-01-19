eed338f235c47909e223c04914e10a09a7499119
Diana Buffone <diana.pamelaet@gmail.com>
2021-01-18 19:37:33 -0500

    Add working features

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -5,14 +5,12 @@ require_relative 'offset'
 in_file = File.open(ARGV[0], 'r')
 to_encrypt = File.read(in_file)
 
-cipher = Enigma.new#(to_encrypt)
-cipher.encrypt(to_encrypt)
+cipher = Enigma.new
+apply_cipher = cipher.encrypt(to_encrypt)
 
 out_file = File.open(ARGV[1], 'w')
-# out_file.encrypt(in_file)
+out_file.write(apply_cipher[:encryption])
 
-p "Created #{ARGV[0]} with the key #{cipher.key.number} and date #{cipher.date.date}"
+p "Created #{ARGV[1]} with the key #{cipher.key.number} and date #{cipher.date.date}"
 in_file.close
 out_file.close
-
-# the interpolated sections are not working.
--- lib/enigma.rb
+++ lib/enigma.rb
@@ -13,7 +13,7 @@ class Enigma
     @date    = Offset.new
   end
 
-  def encrypt(message, key, date)
+  def encrypt(message, key = @key.number, date = @date.date)
     character_set = ("a".."z").to_a << " "
 
     hash1 = @key.calculate_key(key)
@@ -47,17 +47,73 @@ class Enigma
               index_d = character_set.index(array_of_4[3])
               new_ind_d = (index_d + shift_d).modulo(27)
               new_string << character_set[new_ind_d]
+            else
+              array_of_4.each do |letter|
+                if !character_set.include?(letter)
+                  index = letter.index(letter)
+                    new_string << letter[index]
+                end
+              end
             end
           end
-        end
-      elsif character_set.include?(char) == false
-        array_of_4.each do |char|
-          char
         end
       end
     end
     {
       encryption: new_string.join,
+      key:        key,
+      date:       date
+   }
+  end
+
+  def decrypt(cyphertext, key, date = @date.date)
+    character_set = ("a".."z").to_a << " "
+
+    hash1 = @key.calculate_key(key)
+    hash2 = @date.calculate_offset(date)
+    shifts_hash = hash1.merge(hash2) do |hkeys, value1, value2|
+      value1 + value2
+    end
+
+    shift_a = shifts_hash[:a]
+    shift_b = shifts_hash[:b]
+    shift_c = shifts_hash[:c]
+    shift_d = shifts_hash[:d]
+
+    to_decrypt = cyphertext.downcase.chars.each_slice(4).to_a
+    new_string = []
+
+    to_decrypt.map do |array_of_4|
+      if character_set.include?(array_of_4[0])
+        index_a = character_set.index(array_of_4[0])
+        new_ind_a = (index_a - shift_a).modulo(27)
+        new_string << character_set[new_ind_a]
+        if character_set.include?(array_of_4[1])
+          index_b = character_set.index(array_of_4[1])
+          new_ind_b = (index_b - shift_b).modulo(27)
+          new_string << character_set[new_ind_b]
+          if character_set.include?(array_of_4[2])
+            index_c = character_set.index(array_of_4[2])
+            new_ind_c = (index_c - shift_c).modulo(27)
+            new_string << character_set[new_ind_c]
+            if character_set.include?(array_of_4[3])
+              index_d = character_set.index(array_of_4[3])
+              new_ind_d = (index_d - shift_d).modulo(27)
+              new_string << character_set[new_ind_d]
+            else
+              array_of_4.each do |letter|
+                if !character_set.include?(letter)
+                  index = letter.index(letter)
+                    new_string << letter[index]
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+    {
+      decryption: new_string.join,
       key:        key,
       date:       date
    }
